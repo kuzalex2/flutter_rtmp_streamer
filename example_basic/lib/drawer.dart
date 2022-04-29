ffc8e3fe5051eda922884390e2aff30230faeeb8
Alexey Kuznetsov <me@kuzalex.com>
2022-04-29 09:59:33 +0400

    SettingsRow widget

--- example_basic/lib/drawer.dart
+++ example_basic/lib/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:example_basic/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rtmp_streamer/flutter_rtmp_streamer.dart';
+import 'package:unicons/unicons.dart';
 
 
 
@@ -23,6 +24,7 @@ class CameraSettingsDrawer extends StatelessWidget {
             return const Loader();
           }
 
+          // bool disabled = true;
           bool disabled = false;
 
           return FutureBuilder<BackAndFrontResolutions>(
@@ -38,37 +40,82 @@ class CameraSettingsDrawer extends StatelessWidget {
 
 
               // return Text("aaq ${snapshot.data}");
-              return ListView(children: [
-                buildEntityRow2(disabled, Icons.timelapse,
-                  "Video size (${snapshot.data!.back[0].toString()})",
-                      () {
-                    // Navigator.of(context).push(
-                    //     MaterialPageRoute(
-                    //         builder: (BuildContext context) => ListDrawer<Resolution>(
-                    //           title: "Video size",
-                    //           list: availableResolutions,
-                    //           activeItem: widget._controller.initialParams.resolution,
-                    //           onNewActiveItem: (i) {
-                    //             widget._controller.initialParams.resolution = i;
-                    //             setState(() {});
-                    //           },
-                    //         )
-                    //     )
-                    // );
-                  },
+              return ListView(children:  [
+
+
+                SettingsRow(
+                  left: const Icon(UniconsLine.image_resize_landscape),
+                  title: Text("Video size (${snapshot.data!.back[0].toString()})"),
+                  onTap: disabled ? null : () {},
+                  right: const Icon(Icons.arrow_right),
+
                 ),
 
               ],);
             }
-          );
-          // stateStreamSnap.data!.streamResolution;
-          return ListView(
-
           );
         }
       ),
     );
   }
 }
+
+
+
+
+
+class SettingsRow extends StatelessWidget {
+  final Widget? left;
+  final Widget? title;
+  final Widget? right;
+  final Function()? onTap;
+
+
+  const SettingsRow({
+    Key? key,
+    this.left,
+    this.title,
+    this.right,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: IconTheme(
+          data: onTap != null ? const IconThemeData(color: Colors.black) : const IconThemeData(color: Colors.black45),
+          child: DefaultTextStyle(
+            style: onTap != null ? const TextStyle(color: Colors.black) : const TextStyle(color: Colors.black45),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          margin: const EdgeInsets.only(right: 8),
+                          child: left,
+                      ),
+                      title ?? const SizedBox.shrink(),
+                    ],
+                  ),
+                ),
+                right ?? const SizedBox.shrink(),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+
+
 
 
